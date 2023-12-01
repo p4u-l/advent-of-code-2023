e4964d8d9045799d0aadc0a16a083ed28e1e76d3
p4u-l <76221859+p4u-l@users.noreply.github.com>
2023-12-02 00:24:41 +0100

    day01: further cleanup to improve readability

--- day01/day01.py
+++ day01/day01.py
@@ -6,13 +6,19 @@ with open("input", "r") as f:
 digit_strings = {s: str(i+1) for i, s in enumerate(("one", "two", "three", "four", "five", "six", "seven", "eight", "nine"))}
 
 def first_last(l):
-    return (l[0], l[-1])
+    return l[0], l[-1]
 
 def combine_digits(a, b):
     return int(a + b)
 
-print("Part 1:", sum(combine_digits(*first_last(re.findall(r'\d', line))) for line in file_input))
+p1_sum = sum(combine_digits(*first_last(re.findall(r'\d', l))) for l in file_input)
+print("Part 1:", p1_sum)
 
-p2_numbers = [re.findall("(?=(\d|" + "|".join(digit_strings.keys()) + "))", line) for line in file_input]
-print("Part 2:", sum(combine_digits(*map(lambda x: digit_strings.get(x, x), first_last(nums))) for nums in p2_numbers))
+p2_sum = 0
+
+for l in file_input:
+    nums = re.findall(rf"(?=(\d|{'|'.join(digit_strings.keys())}))", l)
+    p2_sum += combine_digits(*map(lambda x: digit_strings.get(x, x), first_last(nums)))
+
+print("Part 2:", p2_sum)
 
